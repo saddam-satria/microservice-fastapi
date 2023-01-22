a52088dd1cad284899e24256e5bc5d0b03a6757b
saddam-satria <ardhisaddam@gmail.com>
2023-01-22 20:31:32 +0700

    update update operation

--- product-service/models.py
+++ product-service/models.py
@@ -49,7 +49,7 @@ class Product(Base):
         self.tags = tags
 
     def __repr__(self) -> str:
-        return f"{self.name} {self.price} {self.description} {self.isFavorite} {self.isFlashSale} {self.discount} {self.discount_type} {self.likes} {self.image} {self.owner} {self.productLegal} {self.tags}" 
+        return f"{self.name} {self.price} {self.description} {self.isFavorite} {self.isFlashSale} {self.discount} {self.discount_type} {self.likes} {self.image} {self.owner} {self.productLegal} {self.tags} {self.product_category_id}" 
 
 
 @event.listens_for(Product, "before_insert")
--- product-service/routes.py
+++ product-service/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException,status
-from database import session
-from models import Product,Category
+from database import session,engine
+from models import Product,Category, Base
 from helpers.response import getReponse
 from schemas import ProductSchema, ProductSchemaUpdate
 from repository import getProductByID,getProductByQuery,getCategoryByName
@@ -57,26 +57,39 @@ def getProduct(productID):
 def updateProduct(productID, productData : ProductSchemaUpdate):
     productFilter = session.query(Product).filter(Product.productID == productID)
     product = productFilter.first()
-
+  
     if product is None: 
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=getReponse(serviceName="update product", errorMessage="product not found", payload=productID,statusCode=status.HTTP_404_NOT_FOUND, responseStatus="error"))
 
-    product = productFilter.update({
-        Product.name : productData.name if productData.name else product.name,
-        Product.price : productData.price if productData.price else product.price,
-        Product.owner : productData.owner if productData.owner else product.owner,
-        Product.category : productData.category if productData.category else product.category, 
-        Product.description : productData.description if productData.description else product.description,
-        Product.discount : productData.discount if productData.discount else product.discount, 
-        Product.discount_type : productData.discount_type if productData.discount_type else product.discount_type,
-        Product.image : productData.image if productData.image else product.image,
-        Product.isFlashSale : productData.isFlashSale if productData.isFlashSale else product.isFlashSale,
-        Product.isFavorite : productData.isFavorite if productData.isFavorite else product.isFavorite,
-        Product.likes : productData.likes if productData.likes else product.likes,
-        Product.tags : productData.tags if productData.tags else product.tags,
-        Product.productLegal : productData.productLegal if productData.productLegal else product.productLegal,
-        Product.updatedAt : datetime.datetime.now()
-    })
+    product.name = productData.name if productData.name else  product.name
+    product.price = productData.price if productData.price else  product.price
+    product.owner = productData.owner if productData.owner else  product.owner
+    product.description = productData.description if productData.description else  product.description
+    product.discount = productData.discount if productData.discount else  product.discount
+    product.discount_type = productData.discount_type if productData.discount_type else  product.discount_type
+    product.image = productData.image if productData.image else  product.image
+    product.isFlashSale = productData.isFlashSale if productData.isFlashSale else  product.isFlashSale
+    product.isFavorite = productData.isFavorite if productData.isFavorite else  product.isFavorite
+    product.likes = productData.likes if productData.likes else  product.likes
+    product.tags = productData.tags if productData.tags else  product.tags
+    product.productLegal = productData.productLegal if productData.productLegal else  product.productLegal
+    product.updatedAt = datetime.datetime.now()
+  
+    if productData.category:
+        category = getCategoryByName(productData.category)
+        prevCategory = session.query(Category).filter(Category.categoryID ==product.product_category_id[0].categoryID).first()
+        if category is None:
+            category = Category(productData.category)
 
+
+        
+        product.product_category_id.remove(prevCategory)
+        product.product_category_id.append(category)
+
+
+    session.flush()
+    session.commit()
+    
+   
 
     return getReponse(serviceName="update product", data=product, payload=productData)
