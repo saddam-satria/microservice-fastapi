ed12cf0e637ce603d403f0d9d8ce70b49e9702b3
saddam-satria <ardhisaddam@gmail.com>
2023-01-25 15:40:04 +0700

    update cors

--- product-service/main.py
+++ product-service/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from route import routing
 from database import engine
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 
 app = FastAPI(docs_url="/doc")
 routing(app)
+
+origins = ["*"]
+
+app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
 
 
 @app.on_event("startup")
